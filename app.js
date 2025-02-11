a15612f2af0497c7816fe948eba6ae587309e584
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2025-02-10 16:20:49 -0800

    added error handler

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const btn = document.querySelector(".btn");
 const paragraph = document.querySelector(".result");
+const url = "https://icanhazdadjoke.com/";
 
-btn.addEventListener("click", async () => {
-  const response = await fetch("https://icanhazdadjoke.com/", {
-    headers: {
-      Accept: "application/json",
-      "User-Agent": "project api site",
-    },
-  });
-
-  const data = await response.json();
-  paragraph.textContent = data.joke;
+btn.addEventListener("click", () => {
+  fetchJoke();
 });
+
+async function fetchJoke() {
+  paragraph.textContent = "Loading...";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "project api site",
+      },
+    });
+    if (!response.ok) {
+      console.log(response);
+
+      throw new Error(response.status);
+    }
+    const data = await response.json();
+    paragraph.textContent = data.joke;
+  } catch (err) {
+    paragraph.textContent = `There was an error ${err.message}`;
+  }
+}
+fetchJoke();
